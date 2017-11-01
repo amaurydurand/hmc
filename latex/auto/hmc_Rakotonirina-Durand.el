8c082c2819b64d08e46d16337310fb56cde45132
Amaury Durand <durand_amaury@hotmail.fr>
2017-11-01 16:20:06 +0100

    debut

---
+++ latex/auto/hmc_Rakotonirina-Durand.el
@@ -0,0 +1,20 @@
+(TeX-add-style-hook
+ "hmc_Rakotonirina-Durand"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "10pt" "a4paper")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art10"
+    "init"
+    "defs"
+    "color")
+   (TeX-add-symbols
+    "red")
+   (LaTeX-add-labels
+    "thm:ergodic")
+   (LaTeX-add-bibliographies
+    "ref"))
+ :latex)
+
---
+++ latex/auto/ref.el
@@ -0,0 +1,8 @@
+(TeX-add-style-hook
+ "ref"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "Neal-hmc"
+    "Douc-mc"))
+ :bibtex)
+
